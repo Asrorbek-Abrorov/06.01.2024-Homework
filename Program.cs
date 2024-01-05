7cf2ea77ef6afa7fd9b185875c63665807b8b63f
Asrorbek Abrorov <138248626+Asrorbek-Abrorov@users.noreply.github.com>
2024-01-06 00:40:05 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Spectre.Console;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string originalDirectory;
+
+        originalDirectory = AnsiConsole.Ask<string>("Write the name of the directory that you want to change : ");
+        
+        
+        
+        string originalDirectoryPath = $"../../../../Files_1_homework/{originalDirectory}";
+        
+        if (!Directory.Exists(originalDirectoryPath))
+        {
+            Console.WriteLine("Directory does not exist.");
+            return;
+        }
+        
+        string newDirectoryName;
+
+        newDirectoryName = AnsiConsole.Ask<string>("Enter the new name for the directory:");
+        
+        Directory.CreateDirectory($"../../../../Files_1_homework/{newDirectoryName}");
+        
+        Directory.Delete($"../../../../Files_1_homework/{originalDirectory}");
+
+        Console.WriteLine("Changed successfully!");
+    }
+}
